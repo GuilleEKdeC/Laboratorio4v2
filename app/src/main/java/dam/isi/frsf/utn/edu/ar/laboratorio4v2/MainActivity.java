6a14283cbaf1dfe37a693656d7a044a554ce6af2
Guille <gecian@yahoo.com.ar>
2016-11-11 00:17:05 -0300

    Probando

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio4v2/MainActivity.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio4v2/MainActivity.java
@@ -2,12 +2,35 @@ package dam.isi.frsf.utn.edu.ar.laboratorio4v2;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.SwitchCompat;
+import android.util.Log;
+import android.view.View;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.Arrays;
+
 import android.widget.Toast;
+
+import dam.isi.frsf.utn.edu.ar.laboratorio4v2.modelo.Ciudad;
+import dam.isi.frsf.utn.edu.ar.laboratorio4v2.utils.FormBusqueda;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     //  OnNavigationItemSelectedListener: listener para manejar las selecciones de los ítems del NavigationView
@@ -30,36 +53,31 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); //Crea una nueva variable Toolbar tomando como referencia la definida en app_bar_main.xml
+        setSupportActionBar(toolbar); // indica a Android que vamos a utilizar nuestra propia Toolbar.
 
-        /*Se crea una nueva variable Toolbar tomando como referencia la definida en app_bar_main.xml y se utiliza
-          el método setSupportActionBar para indicarle a Android que vamos a utilizar nuestra propia Toolbar.*/
-/*        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-*/
-        /*Se crea una nueva variable DrawerLayout (contenedor de nuestro "contenido Principal" y de nuestro "navegador"),
-          tomando como referencia la definida en activity_main.xml.*/
-  //      DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout); //el activity_main.xml
-        /*Compatibiliza el Drawer Layout con la barra de acciones (Toolbar)*/
- //       ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close){
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout); //Crea una nueva variable DrawerLayout (contenedor de nuestro "contenido Principal" y de nuestro "navegador"), el activity_main.xml
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close){//Compatibiliza el Drawer Layout con la barra de acciones (Toolbar)
         /*******************Agregado*******************/
- /*          public void onDrawerClosed(View drawerView) {
+           public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
                 getSupportActionBar().setTitle("Después de cerrar el Navegador (ManiActivity)");//cambia el título a la toolbar
                 invalidateOptionsMenu(); //indica a Android que los contenidos del menú han cambiado y que el menú debe ser redibujado. Crea una llamada al método onPrepareOptionsMenú
             }
 
- /*          public void onDrawerOpened(View drawerView) {
+           public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
                 getSupportActionBar().setTitle("Antes de abrir el Navegador (ManiActivity)"); //cambia el título a la toolbar
                 invalidateOptionsMenu(); //indica a Android que los contenidos del menú han cambiado y que el menú debe ser redibujado. Crea una llamada al método onPrepareOptionsMenú
-            }*/
+            }
         /****************Fin de Agregado****************/
- //       };
+        };
         //drawer.setDrawerListener(toggle); //le setea un listenner al DrawerLayout //línea ORIGINAL pero método deprecated => cambié por addDrawerListener
-/*        drawer.addDrawerListener(toggle); //le setea un listenner al DrawerLayout   // línea AGREGADA
+        drawer.addDrawerListener(toggle); //le setea un listenner al DrawerLayout   // línea AGREGADA
         toggle.syncState(); //Sirve para sincronizar el "Indicador" (las tres barritas horizontales que despliega al navegador) del Drawer Layout
         toggle.setDrawerIndicatorEnabled(true); // setea que el Indicador esté visible // línea AGREGADA
-*/
+
         /*Se crea una nueva variable NavigationView, tomando como referencia la definida en activity_main.xml.*/
 /*        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this); //le setea un listener
